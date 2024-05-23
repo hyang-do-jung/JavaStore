a49449130f3592a372ef35b4f46a13510e010325
jhd <youda0134@naver.com>
2024-05-23 13:08:35 +0900

    first_commit

---
+++ uasability/src/uasability/main_code.java
@@ -0,0 +1,12 @@
+package uasability;
+
+public class main_code {
+	public static void main(String[] args)
+	{
+		int cc = 0;
+		process go = new process();
+		
+		cc = go.return_type(2, 3);
+		System.out.println(cc);
+	}
+}
---
+++ uasability/src/uasability/process.java
@@ -0,0 +1,12 @@
+package uasability;
+
+public class process {
+	public int return_type(int a,int b)
+	{
+		int c = 0;
+		
+		c = a+b;
+		
+		return c;
+	}
+}
